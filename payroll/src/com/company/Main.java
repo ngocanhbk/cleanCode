aed6d9671a26a885bc63239eb6a6cf50d65fc83e
ngocanhbk <32104507+ngocanhbk@users.noreply.github.com>
2020-12-17 15:04:04 +0700

    Update Main.java

--- payroll/src/com/company/Main.java
+++ payroll/src/com/company/Main.java
@@ -1,80 +1,67 @@
 package com.company;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
+/**
+ * @author buithingocanh
+ */
 public class Main {
-    private static final String COMMA_DELIMITER =",";
 
     public static void main(String[] args) {
-        String csvFileLink = "/Users/LA-PM/Desktop/myweb/payroll/input/employee.csv";
-        Employee employee = new Employee("ngoc anh","19970923","engineer",	"20200216","8000000");
-        System.out.println(employee.getAge(employee.getDob()));
-        long day = employee.getWorkingDays(employee.getStartDate());
-        System.out.println(day);
-        System.out.println("year="+(day/365)+" month="+(day/30 - 12* (day/365)));
+        String csvFileLink = "../payroll/input/employee.csv";
 
         List<Employee> list = readFileCSV(csvFileLink);
-        for (Employee emp :list  ) {
-            System.out.println(emp.toString());
-
+        for (Employee emp : list) {
+            Map<String, Integer> mapYearAndMonth = emp.getWorkingDays(emp.getStartDate());
+            System.out.println("Employee{" +
+                    "name:'" + emp.getName() + '\'' +
+                    ", age:'" + emp.getAge(emp.getDob()) + '\'' +
+                    ", rol:'" + emp.getRol() + '\'' +
+                    ", workingDay:'" + mapYearAndMonth.get("year") + " years " + mapYearAndMonth.get("month") + " months" + '\'' +
+                    ", startSal='" + emp.getStartSal() + '\'' +
+                    '}');
         }
-
-        // write your code here
     }
 
-    private static List<Employee>  readFileCSV(String csvFile){
-        BufferedReader br = null;
+    /**
+     * method read file .csv
+     * 
+     * @param csvFile - link file
+     * @return data in file
+     */
+    private static List<Employee> readFileCSV(String csvFile) {
+        BufferedReader dataBufferReader = null;
         String line = "";
         String cvsSplitBy = ",";
         List<Employee> employees = new ArrayList<Employee>();
-
         try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-
+            dataBufferReader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), "UTF8"));
+            while ((line = dataBufferReader.readLine()) != null) {
                 // use comma as separator
                 String[] data = line.split(cvsSplitBy);
-                employees.add(new Employee(data[0],data[1],data[2],data[3],data[4]));
-
+                employees.add(new Employee(data[0], data[1], data[2], data[3], data[4]));
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (br != null) {
+            if (dataBufferReader != null) {
                 try {
-                    br.close();
+                    dataBufferReader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-
-
-
         return employees;
     }
-
-
-    private static List<String> getRecordFromLine(String line) {
-        List<String> values = new ArrayList<String>();
-        Scanner rowScanner = new Scanner(line);
-        try {
-            rowScanner.useDelimiter(COMMA_DELIMITER);
-            while (rowScanner.hasNext()) {
-                values.add(rowScanner.next());
-            }
-        } finally {
-            rowScanner.close();
-        }
-        return values;
-    }
-
 
 }
