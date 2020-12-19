51d9b947515f88ed1a9cc88e6d10705c03027f91
Your Name <you@example.com>
2020-12-19 10:21:05 +0700

    commit

---
+++ payroll/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+M
+src/com/company/Employee.java,9\d\9d1aaa209f287571733d747720ab748c8b57559e
+B
+input/employee.csv,6\e\6ea37fa18c44c0203950447ce145738c34abdf90
+I
+src/com/company/Main.java,4\a\4a831b10439099d44774d0ccc387e808deffaeab
--- payroll/src/com/company/Main.java
+++ payroll/src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,18 +14,20 @@ import java.util.Map;
  * @author buithingocanh
  */
 public class Main {
-    private final static double COEFFICIENTS_SALARY = 1.06;
-    public static void main(String[] args) {
+    final static double COEFFICIENTS_SALARY = 1.06;
+
+    public static void main(String[] args) throws IOException {
         String csvFileLink = "../payroll/input/employee.csv";
 
         List<Employee> list = readFileCSV(csvFileLink);
         for (Employee emp : list) {
             Map<String, Integer> mapYearAndMonth = emp.getWorkingDays(emp.getStartDate());
-            long nowSal = (long) (Long.valueOf(emp.getStartSal())* Math.pow(COEFFICIENTS_SALARY, mapYearAndMonth.get("year"))); 
+            long nowSal = (long) (Long.valueOf(emp.getStartSal()) * Math.pow(COEFFICIENTS_SALARY, mapYearAndMonth.get("year")));
             System.out.println("Employee {" +
                     "name:'" + emp.getName() + '\'' +
+//                    ", birthday:'" + emp.getDob() + '\'' +
                     ", age:'" + emp.getAge(emp.getDob()) + '\'' +
-                    ", rol:'" + emp.getRol() + '\'' +
+//                    ", rol:'" + emp.getRol() + '\'' +
                     ", workingDay:'" + mapYearAndMonth.get("year") + " years " + mapYearAndMonth.get("month") + " months" + '\'' +
                     ", startSal='" + emp.getStartSal() + '\'' +
                     ", nowSal='" + nowSal + '\'' +
@@ -34,26 +37,22 @@ public class Main {
 
     /**
      * method read file .csv
-     * 
+     *
      * @param csvFile - link file
      * @return data in file
      */
-    private static List<Employee> readFileCSV(String csvFile) {
+    private static List<Employee> readFileCSV(String csvFile) throws FileNotFoundException, IOException {
         BufferedReader dataBufferReader = null;
         String line = "";
         String cvsSplitBy = ",";
-        List<Employee> employees = new ArrayList<Employee>();
+        List<Employee> employees = new ArrayList<>();
         try {
-            dataBufferReader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), "UTF8"));
+            dataBufferReader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), StandardCharsets.UTF_8));
             while ((line = dataBufferReader.readLine()) != null) {
                 // use comma as separator
                 String[] data = line.split(cvsSplitBy);
                 employees.add(new Employee(data[0], data[1], data[2], data[3], data[4]));
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         } finally {
             if (dataBufferReader != null) {
                 try {
